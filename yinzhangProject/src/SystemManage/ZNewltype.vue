ffe86c0edb5c49fff8a4f06e0a317747f08b291d
ganxianjun <2431808449@qq.com>
2018-08-25 14:55:03 +0800

    111

---
+++ yinzhangProject/src/SystemManage/ZNewltype.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="Zquanxian">
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="所属单位" style="text-align:left;">
+    <el-radio v-model="form.radio" label="1">备选项</el-radio>
+    <el-radio v-model="form.radio" label="2">备选项</el-radio>
+  </el-form-item>
+  <el-form-item label="所属单位">
+    <el-input v-model="form.name" ></el-input>
+  </el-form-item>
+  <el-form-item label="印章尺寸" style="text-align:left;">
+    <el-input v-model="form.name" style="display:inline-block; width:20%;"></el-input>*<el-input style="width:20%;display:inline-block" v-model="form.name" ></el-input>
+  </el-form-item>
+  <el-form-item label="是否激活">
+    <el-select v-model="form.region" placeholder="请选择活动区域">
+      <el-option label="区域一" value="shanghai"></el-option>
+      <el-option label="区域二" value="beijing"></el-option>
+    </el-select>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="Zbaocun">新建审批类型</el-button>
+    <el-button @click="Zbaocun">取消</el-button>
+  </el-form-item>
+</el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+                form: {
+                name: '',
+                region: '',
+                type: [],
+                radio: '1'
+                }
+
+        }
+    },
+     methods: {
+      Zbaocun(){
+        this.$router.push({path:'/component/GSystemSetup'})
+      }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .Zquanxian{
+        height:100%; widht:100%;
+        background:#fff;
+        text-align:center;
+        .el-form{
+            display:inline-block;
+            width:60%;
+            padding-top:10px;
+        }
+        .el-select{
+            width:100%;
+        }
+    }
+</style>
---
+++ yinzhangProject/src/SystemManage/Zanquanshenjis.vue
@@ -0,0 +1,179 @@
+<template>
+<div class="Zanquan">
+  <div class="Zanquans">
+    <el-row>
+      <span>系统管理员日志</span>
+      <el-button type="info" plain @click="ZNewltype" >进入</el-button>
+  </el-row>
+    <el-table
+      :data="tableData"
+      stripe>
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    </div>
+    <div class="Zanquans">
+      <el-row>
+          <span>安全管理员日志</span>
+          <el-button type="info" plain @click="ZNewltype" >进入</el-button>
+      </el-row>
+          <el-table
+      :data="tableData"
+      stripe>
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+      </div>
+      <div class="Zanquans">
+        <el-row>
+            <span>制作印章人日志</span>
+            <el-button type="info" plain @click="ZNewltype" >进入</el-button>
+        </el-row>
+            <el-table
+      :data="tableData"
+      stripe>
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+        </div>
+        <div class="Zanquans">
+          <el-row>
+              <span>印控仪维护日志</span>
+              <el-button type="info" plain @click="ZNewltype" >进入</el-button>
+          </el-row>
+      <el-table
+      :data="tableData"
+      stripe>
+      <el-table-column
+        prop="date"
+        label="日期">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+          </div>
+ </div>
+</template>
+<style>
+  .el-table{
+      height:80%;
+    }
+    .el-table__body tr{
+      height:20%;
+    }
+    .el-table td{
+      padding:8px 0;
+    }
+</style>
+<style lang="scss" scoped>
+.Zanquan{
+	width: 100%;
+	height: 100%;
+  min-width:736px;
+}
+  .Zanquans{
+    width:45%; height:50%;
+    min-width:360px;
+    display:inline-block;
+    .el-table{
+      height:80%;
+      tr{
+        height:20%;
+      }
+      td{
+        padding:0;
+      }
+    }
+    button{
+      float:right;
+    }
+    span{
+      display:inline-block;
+      heihgt:40px;
+      line-height:40px;
+    }
+  }
+</style>
+
+<script>
+  export default {
+    methods: {
+      ZNewltype(){
+        // tZanquans.$router.push({path:"/component/ZNewltype"})
+      }
+    },
+    data() {
+      return {
+        tableData: [{
+          date: '2',
+          name: '王小虎',
+          address: '上海市普陀'
+        }, {
+          date: '2',
+          name: '王小虎',
+          address: '上海市普陀',
+        }, {
+          date: '20',
+          name: '王小虎',
+          address: '上海市普陀'
+        },{
+          date: '4',
+          name: '王小虎',
+          address: '上海市普陀区',
+        }]
+      }
+    }
+  }
+</script>
